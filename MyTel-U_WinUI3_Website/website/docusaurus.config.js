d331a5917b73d0765a74b4ebc413f94b80b5147b
GID <108791227+GID0317@users.noreply.github.com>
2025-06-17 23:53:38 +0700

    Update docusaurus.config.js

--- MyTel-U_WinUI3_Website/website/docusaurus.config.js
+++ MyTel-U_WinUI3_Website/website/docusaurus.config.js
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://gid0317.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/MyTel-U_WinUI3/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'GID0317',
+  projectName: 'MyTel-U_WinUI3',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
